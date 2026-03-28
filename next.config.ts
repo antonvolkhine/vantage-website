9331c85236ed6b92d8725a8b0ba2acef224e792b
Anton Volkhine <antonvolkhine@MacBook-Pro-von-Anton.fritz.box>
2026-03-28 13:11:43 +0100

    Fix: WOFF2 Font Content-Type Header

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/assets/fonts/:path*.woff2",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "font/woff2",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
